8f3d3303e2d54bebae53783f6cf7a1fef9160297
unknown <sebastienfloreal544@gmail.com>
2025-11-25 21:31:57 -0500

    The calculator

---
+++ new.py
@@ -0,0 +1,11 @@
+print('WELCOME TO MY PYTHOGOREAM THEOREM CALCULATOR')
+print('================================================')
+
+a = (int(input('Input The length of side a: ')))
+
+b = (int(input('Input the length of side b: ')))
+
+c = a**2 + b**2
+
+print('The length of side c is:', c)
+
